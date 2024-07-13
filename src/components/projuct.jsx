f2d38ae196a9d0e30aaa54fd55c261637717f4de
Kunal8615 <113933312+Kunal8615@users.noreply.github.com>
2024-07-13 14:24:23 +0530

    img-margin

--- src/components/projuct.jsx
+++ src/components/projuct.jsx
@@ -5,7 +5,7 @@ function ProductSection({prop,col ,resource,val}) {
    <>
    <div  className={` p-2 content-center border-4 border-solid border-slate-900 flex-wrap flex-row justify-center mt-5 h{ w-full md:justify_center ${prop} gap-10`}>
     <div className=' m-3 mt-10 pb-5'>
-    <img className='m-3  border-4 border-solid border-cyan-500' height={500} width={500} src={resource}></img>
+    <img className=' border-4 border-solid border-cyan-500' height={500} width={500} src={resource}></img>
     </div>
     <div className= ' content-center justify-center '>
     
